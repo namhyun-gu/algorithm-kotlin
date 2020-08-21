77b9c2b79d49f986cd242054d436ed100d4b7347
Namhyun, Gu <mnhan0403@gmail.com>
2020-08-22 02:55:53 +0900

    Update source formatter

--- scripts/src_formatter.py
+++ scripts/src_formatter.py
@@ -1,36 +1,69 @@
-import sys
+from dataclasses import dataclass
+from prompt_toolkit import prompt
+from prompt_toolkit.completion import WordCompleter, PathCompleter
 
-if len(sys.argv) == 0:
-    print("Required source arguments")
+
+@dataclass
+class Type:
+    path: str
+    className: str
+
+    def __init__(self, path: str, className: str) -> None:
+        self.path = path
+        self.className = className
+
+
+class Formatter:
+
+    @staticmethod
+    def format(type: Type, filepath: str):
+        with open(filepath) as file:
+            for line in file.readlines():
+                if Formatter.__has_package(line):
+                    continue
+                if Formatter.__is_comment(line):
+                    continue
+                line = Formatter.__replace_classname(line, type.className)
+                line = line.replace('\n', '')
+                print(line)
+
+    @staticmethod
+    def __has_package(line: str) -> bool:
+        return line.startswith("package")
+
+    @staticmethod
+    def __replace_classname(line: str, newClassName: str) -> str:
+        class_declation = "public class "
+        if line.startswith(class_declation):
+            classname = line[len(class_declation):len(line) - 3]
+            return line.replace(classname, newClassName)
+        else:
+            return line
+
+    @staticmethod
+    def __is_comment(line: str) -> bool:
+        return line.startswith("//") or line.startswith("/**") or line.startswith("**/")
+
+
+types = {
+    'baekjoon': Type(path='src/main/java/baekjoon/', className="Main"),
+    'programmers': Type(path='src/main/java/programmers/', className="Solution")
+}
+
+type_completer = WordCompleter(list(types))
+
+type = prompt('type> ', completer=type_completer, complete_while_typing=True)
+
+if type not in list(types):
+    print("Type must be in %s" % list(types))
     exit()
 
-source = sys.argv[1]
-source = "src/main/java/" + source
+path = prompt('path> ', completer=PathCompleter(
+    get_paths=lambda: [types[type].path],
+    min_input_len=1
+), complete_while_typing=True)
 
-
-def has_package(line: str) -> bool:
-    return line.startswith("package")
-
-
-def replace_classname(line: str) -> str:
-    class_declation = "public class "
-    if line.startswith(class_declation):
-        classname = line[len(class_declation):len(line) - 3]
-        return line.replace(classname, "Main")
-    else:
-        return line
-
-
-def is_comment(line: str) -> bool:
-    return line.startswith("//") or line.startswith("/**") or line.startswith("**/")
-
-
-with open(source) as file:
-    for line in file.readlines():
-        if has_package(line):
-            continue
-        if is_comment(line):
-            continue
-        line = replace_classname(line)
-        line = line.replace('\n', '')
-        print(line)
+if path.endswith(".java"):
+    Formatter.format(type=types[type], filepath=types[type].path + path)
+else:
+    print("Path can only java file")
