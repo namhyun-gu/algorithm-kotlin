a72d9ef8676ab53ed7791f09875fbf5e7ba41aca
Namhyun, Gu <mnhan0403@gmail.com>
2020-08-22 10:47:08 +0900

    Add Validator to prompt

--- scripts/src_formatter.py
+++ scripts/src_formatter.py
@@ -1,6 +1,9 @@
 from dataclasses import dataclass
 from prompt_toolkit import prompt
 from prompt_toolkit.completion import WordCompleter, PathCompleter
+from prompt_toolkit.document import Document
+from prompt_toolkit.validation import Validator, ValidationError
+import os
 
 
 @dataclass
@@ -11,6 +14,12 @@ class Type:
     def __init__(self, path: str, className: str) -> None:
         self.path = path
         self.className = className
+
+
+types = {
+    'baekjoon': Type(path='src/main/java/baekjoon/', className="Main"),
+    'programmers': Type(path='src/main/java/programmers/', className="Solution")
+}
 
 
 class Formatter:
@@ -45,25 +54,35 @@ class Formatter:
         return line.startswith("//") or line.startswith("/**") or line.startswith("**/")
 
 
-types = {
-    'baekjoon': Type(path='src/main/java/baekjoon/', className="Main"),
-    'programmers': Type(path='src/main/java/programmers/', className="Solution")
-}
+class TypeValidator(Validator):
+    def validate(self, document: Document) -> None:
+        type = document.text
+        if type not in list(types):
+            raise ValidationError(message='Type must be in %s' % list(types))
+
+
+class PathValidator(Validator):
+    def validate(self, document: Document) -> None:
+        path = document.text
+        if not path.endswith(".java") and not os.path.exists(path):
+            raise ValidationError(message='Path can only java file')
+
 
 type_completer = WordCompleter(list(types))
 
-type = prompt('type> ', completer=type_completer, complete_while_typing=True)
+type = prompt('type> ',
+              completer=type_completer,
+              complete_while_typing=True,
+              validator=TypeValidator(),
+              validate_while_typing=True)
 
-if type not in list(types):
-    print("Type must be in %s" % list(types))
-    exit()
+path = prompt('path> ',
+              completer=PathCompleter(
+                  get_paths=lambda: [types[type].path],
+                  min_input_len=1
+              ),
+              complete_while_typing=True,
+              validator=PathValidator(),
+              validate_while_typing=True)
 
-path = prompt('path> ', completer=PathCompleter(
-    get_paths=lambda: [types[type].path],
-    min_input_len=1
-), complete_while_typing=True)
-
-if path.endswith(".java"):
-    Formatter.format(type=types[type], filepath=types[type].path + path)
-else:
-    print("Path can only java file")
+Formatter.format(type=types[type], filepath=types[type].path + path)
