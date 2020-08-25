ee80e9afc48517ec971f81e8f2f24d23fd38e5a3
Namhyun, Gu <mnhan0403@gmail.com>
2020-08-25 20:01:08 +0900

    Add support kotlin file

--- scripts/src_formatter.py
+++ scripts/src_formatter.py
@@ -23,34 +23,79 @@ types = {
 
 
 class Formatter:
-
-    @staticmethod
-    def format(type: Type, filepath: str):
+    @classmethod
+    def __load_file__(self, filepath):
         with open(filepath) as file:
             for line in file.readlines():
-                if Formatter.__has_package(line):
-                    continue
-                if Formatter.__is_comment(line):
-                    continue
-                line = Formatter.__replace_classname(line, type.className)
-                line = line.replace('\n', '')
-                print(line)
+                yield line
 
-    @staticmethod
-    def __has_package(line: str) -> bool:
+    @classmethod
+    def __format_line__(self, type: Type, line: str) -> str:
+        raise NotImplementedError
+
+    @classmethod
+    def format(self, filepath: str, type: Type):
+        for line in self.__load_file__(filepath):
+            formatted = self.__format_line__(type=type, line=line)
+            if not formatted.startswith("-"):
+                yield formatted
+
+
+class JavaFormatter(Formatter):
+    @classmethod
+    def __format_line__(self, type: Type, line: str) -> str:
+        if self.__has_package__(line):
+            return "- " + line
+        if self.__is_comment__(line):
+            return "- " + line
+        if self.__is_declare_class__(line):
+            return self.__replace_classname__(line, type.className).replace('\n', '')
+        return line.replace('\n', '')
+
+    @classmethod
+    def __has_package__(self, line: str) -> bool:
         return line.startswith("package")
 
-    @staticmethod
-    def __replace_classname(line: str, newClassName: str) -> str:
-        class_declation = "public class "
-        if line.startswith(class_declation):
-            classname = line[len(class_declation):len(line) - 3]
-            return line.replace(classname, newClassName)
-        else:
-            return line
+    @classmethod
+    def __is_declare_class__(self, line: str) -> bool:
+        return line.startswith("public class ")
 
-    @staticmethod
-    def __is_comment(line: str) -> bool:
+    @classmethod
+    def __replace_classname__(self, line: str, newClassName: str) -> str:
+        classname = line[len("public class "):len(line) - 3]
+        return line.replace(classname, newClassName)
+
+    @classmethod
+    def __is_comment__(self, line: str) -> bool:
+        return line.startswith("//") or line.startswith("/**") or line.startswith("**/")
+
+
+class KotlinFormatter(Formatter):
+    @classmethod
+    def __format_line__(self, type: Type, line: str) -> str:
+        if self.__has_package__(line):
+            return "- " + line
+        if self.__is_comment__(line):
+            return "- " + line
+        if self.__is_declare_class__(line):
+            return self.__replace_classname__(line, type.className).replace('\n', '')
+        return line.replace('\n', '')
+
+    @classmethod
+    def __has_package__(self, line: str) -> bool:
+        return line.startswith("package")
+
+    @classmethod
+    def __is_declare_class__(self, line: str) -> bool:
+        return line.startswith("class ")
+
+    @classmethod
+    def __replace_classname__(self, line: str, newClassName: str) -> str:
+        classname = line[len("class "):len(line) - 3]
+        return line.replace(classname, newClassName)
+
+    @classmethod
+    def __is_comment__(self, line: str) -> bool:
         return line.startswith("//") or line.startswith("/**") or line.startswith("**/")
 
 
@@ -64,8 +109,15 @@ class TypeValidator(Validator):
 class PathValidator(Validator):
     def validate(self, document: Document) -> None:
         path = document.text
-        if not path.endswith(".java") and not os.path.exists(path):
-            raise ValidationError(message='Path can only java file')
+        if not (path.endswith(".java") or path.endswith(".kt")) and not os.path.exists(path):
+            raise ValidationError(message='Path can only java or kotlin file')
+
+
+class RerunValidator(Validator):
+    def validate(self, document: Document) -> None:
+        path = document.text
+        if not (path == 'y' or path == 'n'):
+            raise ValidationError(message='Can only y or n')
 
 
 type_completer = WordCompleter(list(types))
@@ -85,4 +137,24 @@ path = prompt('path> ',
               validator=PathValidator(),
               validate_while_typing=True)
 
-Formatter.format(type=types[type], filepath=types[type].path + path)
+
+def run_formatter(type: Type, filepath: str):
+    formatter: Formatter = None
+    if filepath.endswith(".java"):
+        formatter = JavaFormatter()
+    elif filepath.endswith(".kt"):
+        formatter = KotlinFormatter()
+
+    for line in formatter.format(filepath, type):
+        print(line)
+
+
+rerun = True
+while rerun:
+    run_formatter(types[type], types[type].path + path)
+
+    rerun = prompt('\nRe-run? (y/n) ', validator=RerunValidator(),
+                   validate_while_typing=True)
+
+    if rerun == 'n':
+        rerun = False
