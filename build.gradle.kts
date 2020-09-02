f4bc0d7a296617ee557ed1c6aefff129b35d46d3
Namhyun, Gu <mnhan0403@gmail.com>
2020-09-02 11:14:44 +0900

    Update kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    kotlin("jvm") version "1.3.72"
+    kotlin("jvm") version "1.4.0"
 }
 
 version = "1.0"
@@ -11,8 +11,8 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    testCompile("junit", "junit", "4.12")
-    testCompile("com.github.stefanbirkner:system-rules:1.19.0")
+    testImplementation("junit", "junit", "4.12")
+    testImplementation("com.github.stefanbirkner:system-rules:1.19.0")
 }
 
 tasks {
