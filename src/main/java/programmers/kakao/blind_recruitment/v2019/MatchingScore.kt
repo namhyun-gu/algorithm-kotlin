8750fe200422ae3f71805775e1cf1710cb204d4f
Namhyun, Gu <mnhan0403@gmail.com>
2020-09-10 18:23:14 +0900

    [programmers] Update kakao/blind_recruitment/v2019/MatchingScore

--- src/main/java/programmers/kakao/blind_recruitment/v2019/MatchingScore.kt
+++ src/main/java/programmers/kakao/blind_recruitment/v2019/MatchingScore.kt
@@ -1,118 +1,73 @@
 package programmers.kakao.blind_recruitment.v2019
 
-import java.util.*
-
-// TODO: 테스트 통과 X, Regex로 코드 변경하기
+// Reference: https://leveloper.tistory.com/110
 class MatchingScore {
-    data class Page(val defaultScore: Int, val externalLinkScore: Int) {
-        val linkScore: Double
-            get() = defaultScore / externalLinkScore.toDouble()
-
-        val matchingScore: Double
+    data class Page(val idx: Int, val originUrl: String, val links: List<String>, val defaultScore: Int, var linkScore: Double = 0.0) {
+        val matchingScore
             get() = defaultScore + linkScore
     }
 
-    data class Tag(val name: String, val attributes: Map<String, String>)
+    val pageMap = mutableMapOf<String, Page>()
 
     fun solution(word: String, pages: Array<String>): Int {
-        var maxScore = Double.MIN_VALUE
+        pages.mapIndexed { idx, str -> parsePage(idx, str.toLowerCase(), word.toLowerCase()) }
+                .forEach { pageMap[it.originUrl] = it }
+
+        pageMap.forEach { (_, page) ->
+            page.links.forEach {
+                if (pageMap.containsKey(it)) {
+                    pageMap[it]!!.linkScore += page.defaultScore / page.links.size.toDouble()
+                }
+            }
+        }
+
+        var maxMatchingScore = Double.MIN_VALUE
         var maxIdx = 0
-        pages.map { parsePage(it, word) }.forEachIndexed { index, page ->
-            if (page.matchingScore > maxScore) {
-                maxIdx = index
-                maxScore = page.matchingScore
+        pageMap.values.forEach { page ->
+            if (page.matchingScore > maxMatchingScore) {
+                maxMatchingScore = page.matchingScore
+                maxIdx = page.idx
             }
         }
         return maxIdx
     }
 
-    fun containWord(str: String, word: String): Int {
-        val chars = str.toLowerCase().map {
-            if (it in 'a'..'z') it
-            else ' '
-        }
-        val builder = StringBuilder()
-        chars.forEach { builder.append(it) }
-        var line = builder.toString()
-        var contains = 0
+    fun parsePage(idx: Int, html: String, word: String): Page {
+        val originUrl = findOriginUrl(html)
+        val links = findLink(html)
+        val defaultScore = calculateDefaultScore(html, word)
 
-        val tokens = tokenize(line)
-        tokens.forEach {
-            if (it == word.toLowerCase()) {
-                contains++
-            }
-        }
-        return contains
+        return Page(idx, originUrl, links, defaultScore)
     }
 
-    fun parsePage(page: String, word: String): Page {
-        val lines = page.split("\n")
-        val urls = mutableListOf<String>()
-        var originUrl = ""
+    fun findOriginUrl(html: String): String {
+        val pattern = "<meta property=\"og:url\" content=\"https://(.+?)\"/>".toPattern()
+        val matcher = pattern.matcher(html)
+
+        return if (matcher.find()) matcher.group(1)
+        else ""
+    }
+
+    fun findLink(html: String): List<String> {
+        val pattern = "<a href=\"https://(.+?)\">".toPattern()
+        val matcher = pattern.matcher(html)
+        val links = mutableListOf<String>()
+
+        while (matcher.find()) {
+            links.add(matcher.group(1))
+        }
+        return links
+    }
+
+    fun calculateDefaultScore(html: String, word: String): Int {
+        var idx = html.indexOf(word)
         var defaultScore = 0
-        lines.map { it.trim() }.forEach {
-            parseTags(it).forEach { tag ->
-                if (tag.name == "meta" && tag.attributes.containsKey("content")) {
-                    originUrl = tag.attributes["content"] ?: ""
-                }
-                if (tag.name == "a" && tag.attributes.containsKey("href")) {
-                    urls.add(tag.attributes["href"] ?: "")
-                }
+        while (idx >= 0) {
+            if (!html[idx - 1].isLowerCase() && !html[idx + word.length].isLowerCase()) {
+                defaultScore++
             }
-            defaultScore += containWord(it, word)
+            idx = html.indexOf(word, idx + 1)
         }
-        val externalLinkScore = urls.filter { !it.startsWith(originUrl) }.size
-        return Page(defaultScore, externalLinkScore)
-    }
-
-    fun parseTags(content: String): List<Tag> {
-        val contents = mutableListOf<Tag>()
-        var startIdx = 0
-        var endIdx: Int
-        content.forEachIndexed { idx, c ->
-            when (c) {
-                '<' -> startIdx = idx
-                '>' -> {
-                    endIdx = idx
-                    var tagBody = content.substring(startIdx + 1, endIdx)
-                    if (tagBody.endsWith("/")) {
-                        tagBody = tagBody.removeSuffix("/")
-                    }
-                    val tag = parseTag(tagBody)
-                    contents.add(tag)
-                }
-            }
-        }
-        return contents
-    }
-
-    fun parseTag(tagBody: String): Tag {
-        val attributes = mutableMapOf<String, String>()
-        return if (tagBody.startsWith("/")) {
-            Tag(tagBody.substring(1), attributes)
-        } else {
-            val tokens = tokenize(tagBody)
-            if (tokens.size > 1) {
-                for (idx in 1 until tokens.size) {
-                    val attribute = parseAttribute(tokens[idx])
-                    attributes[attribute.first] = attribute.second
-                }
-            }
-            Tag(tokens.first(), attributes)
-        }
-    }
-
-    fun parseAttribute(attributeToken: String): Pair<String, String> {
-        val (name, value) = attributeToken.split("=")
-        return name to value.removeSurrounding("\"")
-    }
-
-    fun tokenize(s: String): List<String> {
-        val tokenizer = StringTokenizer(s)
-        val tokens = mutableListOf<String>()
-        while (tokenizer.hasMoreTokens()) {
-            tokens.add(tokenizer.nextToken())
-        }
-        return tokens
+        return defaultScore
     }
 }
